9da18bcd3f62e375d7a9c321febff184485d7d9b
Julia-IO <tobetranslated01@gmail.com>
2021-02-19 14:59:19 +0000

    Remove Secret Key

--- django_test/settings.py
+++ django_test/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'yz%v8%&r@r3*wro+&(&jxr4ry!up+rwytff_tw*hkrd6!ig6y%')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
